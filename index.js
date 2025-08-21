97a86d9cfdfc5617b1cc8085ec1c3c62138cd251
Sardorbekdeveloper <davletboyevsardorbek08@gmail.com>
2025-08-21 11:35:21 +0500

    init

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Render! from Najot ta'lim 21.08.2025");
+  res.send("Hello from Najot ta'lim 21.08.2025");
 });
 
 app.listen(PORT, () => {
